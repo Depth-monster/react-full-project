252a59f7133a124e89ffb6c1d5612bbc86819054
Azat <122405130+Depth-monster@users.noreply.github.com>
2023-01-22 01:22:58 +0300

    Update Loader.jsx

--- src/UI/Loader/Loader.jsx
+++ src/UI/Loader/Loader.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import cl from "./Loader.module.css";
-
+//here I had troubles, because i was looking for the class bug. Finally Ive found it and changed to className
 const Loader = () => {
   return (
     <div className={cl.center}>
